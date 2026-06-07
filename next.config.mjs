9a2e3e8c42fd6ab865f5a99c5d92b68338f41e7e
mdshihabshahriar <shihabshahriar922@gmail.com>
2026-06-07 20:56:32 +0600

    signup page implemented

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ['better-auth', '@better-auth/kysely-adapter', 'kysely'],
   images: {
     remotePatterns: [
       {
